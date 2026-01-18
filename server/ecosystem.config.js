826b9e26429a19c475372b940fa157aa84112ac0
GodMode <nusetorfoster77@gmail.com>
2026-01-18 09:30:24 +0000

    Remove obsolete test scripts and system fonts reference

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -6,6 +6,8 @@ module.exports = {
       args: "run dev",
       env: {
         NODE_ENV: "development",
+        ALLOWED_ORIGINS: "http://localhost:3000,https://new.d2q9a2qe08dxgc.amplifyapp.com",
+        API_URL: "https://vd2v0ljumh.execute-api.eu-north-1.amazonaws.com/prod",
       },
     },
   ],

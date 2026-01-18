c2dcbf27e4079861b3fad1043fd0d4d931be47aa
GodMode <nusetorfoster77@gmail.com>
2026-01-18 09:52:14 +0000

    feat: update help text to link to support page instead of email

--- client/src/components/UploadBlockedScreen.tsx
+++ client/src/components/UploadBlockedScreen.tsx
@@ -238,10 +238,9 @@ export default function UploadBlockedScreen({
 
           {/* Help Text */}
           <p className="text-xs sm:text-sm text-gray-500 text-center mt-6">
-            Need help? Contact{' '}
-            <a href="mailto:support@velo.com" className="text-indigo-600 hover:text-indigo-700 font-medium">
-              support@velo.com
-            </a>
+            <p className="text-sm text-blue-700">
+                Need help? <Link href="/help" className="font-semibold underline hover:text-blue-900">Contact our support team</Link>
+              </p>
           </p>
         </div>
       </div>

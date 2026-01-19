57e3009e2c043e0e02b859273908567bf3f09d2d
GodMode <nusetorfoster77@gmail.com>
2026-01-19 06:52:05 +0000

    feat: update allowed origins in ecosystem config for CORS support

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
       args: "run dev",
       env: {
         NODE_ENV: "development",
-        ALLOWED_ORIGINS: "http://localhost:3000,https://new.d2q9a2qe08dxgc.amplifyapp.com",
+        ALLOWED_ORIGINS: "http://localhost:3000,https://new.d2q9a2qe08dxgc.amplifyapp.com,https://velolink.club,https://www.velolink.club",
         API_URL: "https://vd2v0ljumh.execute-api.eu-north-1.amazonaws.com/prod",
       },
     },

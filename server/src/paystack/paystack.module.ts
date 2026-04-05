8fbf3d5ffeaac2185125ea740dc676a5a05beddc
GodMode <nusetorfoster77@gmail.com>
2026-04-05 23:17:15 +0000

    feat: implement Paystack module with service, controller, and necessary dependencies

--- server/src/paystack/paystack.module.ts
+++ server/src/paystack/paystack.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PaystackService } from './paystack.service';
 import { PaystackController } from './paystack.controller';
+import { PrismaModule } from '../prisma/prisma.module';
+import { EmailModule } from '../email/email.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
+  imports: [PrismaModule, EmailModule, NotificationsModule],
   controllers: [PaystackController],
   providers: [PaystackService],
   exports: [PaystackService],
